cf5b8a7ac5d58e336a608712aa711ffe0193c415
toddtrishatodd <todd.trishatodd@gmail.com>
2026-05-03 10:18:00 -0400

    Add files via upload

---
+++ content/assets/1ao9StEL5G31WYVi/story_content/5q1vnUW7LxT_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":209,"text":"You're working the support queue."},{"start":1799,"text":"3 tickets,"},{"start":2789,"text":"3 different situations."},{"start":4610,"text":"In each one,"},{"start":5610,"text":"relay has already drafted a response."},{"start":8439,"text":"Your job is to decide what to do with it."},{"start":10970,"text":"Go ahead and click continue to look at the first ticket."}]}]};
+    window.globalLoadJsAsset('story_content/5q1vnUW7LxT_transcripts.js', JSON.stringify(data));
+})();
